ddf400fce350f3b943abf29d42e9a67f227db2dc
Purab Shah <purab.shah93@gmail.com>
2024-04-12 22:03:14 +0530

    Fix: load issue

--- hollaex-next-demo/pages/balance.js
+++ hollaex-next-demo/pages/balance.js
@@ -9,6 +9,7 @@ const BalancePage = () => {
   const [totalBalance, setTotalBalance] = useState(0);
   const [coinList, setCoinList] = useState([]);
   const [constantsData, setConstantsData] = useState(null);
+  const [balanceData, setBalanceData] = useState(null);
 
   const updateCoinList = ({ balance }) => {
     const resArr = [];
@@ -17,31 +18,34 @@ const BalancePage = () => {
     Object.keys(balance).map((key) => {
       const { native_currency_value, original_value } = balance[key];
       const coinData = constantsData?.coins[key];
-      amt+=native_currency_value;
+      amt += native_currency_value;
       resArr.push({
         id: key,
         name: coinData?.fullname,
         logo: coinData?.logo,
-        amount: original_value
+        amount: original_value,
       });
     });
 
     setCoinList(resArr);
     setTotalBalance(amt);
-  }
+  };
 
   const getBalance = async () => {
     try {
-      const constants = await balanceService.getConstants();
-      const date = new Date();
-      const formattedDate = date.toISOString();
-      const balanceData = await balanceService.getBalance(
-        formattedDate,
-        formattedDate
-      );
-      console.log('constants',constants);
-      setConstantsData(constants);
-      balanceData.data[0] && updateCoinList(balanceData.data[0])
+      if (!constantsData) {
+        const constants = await balanceService.getConstants();
+        setConstantsData(constants);
+      }
+      if (!balanceData) {
+        const date = new Date();
+        const formattedDate = date.toISOString();
+        const balanceResult = await balanceService.getBalance(
+          formattedDate,
+          formattedDate
+        );
+        setBalanceData(balanceResult.data);
+      }
     } catch (e) {
       enqueueSnackbar(e, {
         variant: "error",
@@ -51,62 +55,65 @@ const BalancePage = () => {
 
   useEffect(() => {
     getBalance();
-  }, []);
+  }, [constantsData]);
+
+  useEffect(() => {
+    balanceData?.length && updateCoinList(balanceData[0]);
+  }, [balanceData]);
 
   return (
     <PageLayout>
-
-    <div className="flex justify-center h-screen w-[80vw] text-black bg-gray-50">
-      <div className="w-full p-8 bg-white rounded shadow-lg">
-        <h1 className="text-3xl font-bold mb-6">
-          Total Balance: {totalBalance.toFixed(2)} USDT
-        </h1>
-        <input
-          className="w-full px-4 py-2 mb-6 border border-gray-300 rounded-md"
-          type="text"
-          placeholder="Search currencies..."
-        />
-        <table className="w-full border border-gray-300 rounded-md">
-          <thead>
-            <tr>
-              <th className="py-2 px-4 bg-gray-100 text-left">Currency</th>
-              <th className="py-2 px-4 bg-gray-100 text-left">Amount</th>
-              <th className="py-2 px-4 bg-gray-100 text-left">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {coinList.map((currency) => (
-              <tr key={currency.id}>
-                <td className="py-2 px-4 flex">
-                <Image 
-                  alt={currency.name}
-                  src={currency.logo}
-                  width={25}
-                  height={25}
-                  className="mr-3"
-                />
-                {currency.name}
-                </td>
-                <td className="py-2 px-4">{currency.amount}</td>
-                <td className="py-2 px-4">
-                  <Link
-                    href="/deposit"
-                    className="text-blue-500 cursor-pointer"
-                  >
-                    <button className="px-3 py-1 mr-2 border border-blue-800 text-blue-800 rounded-md cursor-pointer">
-                      Deposit
-                    </button>
-                  </Link>
-                  <button className="px-3 py-1 border border-red-500 text-red-500 rounded-md">
-                    Withdraw
-                  </button>
-                </td>
+      <div className="flex justify-center h-screen w-[80vw] text-black bg-gray-50">
+        <div className="w-full p-8 bg-white rounded shadow-lg">
+          <h1 className="text-3xl font-bold mb-6">
+            Total Balance: {totalBalance.toFixed(2)} USDT
+          </h1>
+          <input
+            className="w-full px-4 py-2 mb-6 border border-gray-300 rounded-md"
+            type="text"
+            placeholder="Search currencies..."
+          />
+          <table className="w-full border border-gray-300 rounded-md">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 bg-gray-100 text-left">Currency</th>
+                <th className="py-2 px-4 bg-gray-100 text-left">Amount</th>
+                <th className="py-2 px-4 bg-gray-100 text-left">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {coinList.map((currency) => (
+                <tr key={currency.id}>
+                  <td className="py-2 px-4 flex">
+                    <Image
+                      alt={currency.name}
+                      src={currency.logo}
+                      width={25}
+                      height={25}
+                      className="mr-3"
+                    />
+                    {currency.name}
+                  </td>
+                  <td className="py-2 px-4">{currency.amount}</td>
+                  <td className="py-2 px-4">
+                    <Link
+                      href="/deposit"
+                      className="text-blue-500 cursor-pointer"
+                    >
+                      <button className="px-3 py-1 mr-2 border border-blue-800 text-blue-800 rounded-md cursor-pointer">
+                        Deposit
+                      </button>
+                    </Link>
+                    <button className="px-3 py-1 border border-red-500 text-red-500 rounded-md">
+                      Withdraw
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
     </PageLayout>
   );
 };
